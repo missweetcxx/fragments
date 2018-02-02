6e6d917084db70d9ce049937ad080179a4dfc66d
caixiangxiang <caixiangxiang@xiaomi.com>
2018-02-02 15:53:40 +0800

    add counter.py

---
+++ fragments/counter.py
@@ -0,0 +1,27 @@
+import io
+import re
+from os import path
+
+from config import ROOT_PATH
+
+
+class Counter:
+    def __init__(self, path):
+        self.mapping = dict()
+        with io.open(path, encoding='utf-8') as f:
+            data = f.read()
+            words = [s.lower() for s in re.findall("\w+", data)]
+            for word in words:
+                self.mapping[word] = self.mapping.get(word, 0) + 1
+
+    def most_common(self, n):
+        assert n > 0
+        return sorted(self.mapping.items(), key=lambda item: item[1], reverse=True)[:n]
+
+
+if __name__ == '__main__':
+    poem_path = path.join(ROOT_PATH, 'resources/poem.txt')
+    most_common_5 = Counter(poem_path).most_common(5)
+
+    for item in most_common_5:
+        print(item)
