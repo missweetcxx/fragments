60bd08efb65ad968bd021873b8018134c6cece3a
caixiangxiang <caixiangxiang@xiaomi.com>
2018-02-05 18:16:04 +0800

    add code segment to get douban top250 movies

---
+++ segments/douban/config.py
@@ -0,0 +1,9 @@
+from os import path
+
+import yaml
+
+ROOT_PATH = path.abspath(path.join(path.dirname(__file__)))
+CONF_FILE = path.join(ROOT_PATH, 'conf.yaml')
+
+with open(CONF_FILE, 'r') as f:
+    CONFIG = yaml.load(f)
