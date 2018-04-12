f64aa407c5476233cd26f162b405dcf9adb262e6
caixiangxiang [蔡想想] <caixiangxiang@xiaomi.com>
2018-04-12 09:23:46 +0800

    add remove all

--- segments/linked_list/linked_list.py
+++ segments/linked_list/linked_list.py
@@ -6,28 +6,28 @@ from segments.linked_list.node import Node
 class LinkedList(object):
     # set head node None
     def __init__(self):
-        self.head = None
+        self.next = None
 
     # insert new node at start
     def add_head(self, data):
         new_node = Node(data)
-        new_node.set_next(self.head)
-        self.head = new_node
+        new_node.set_next(self.next)
+        self.next = new_node
 
     # insert new node at the end
     def add_tail(self, data):
-        checking = self.head
+        checking = self.next
         while checking is not None:
             if checking.get_next() is None:
                 checking.set_next(Node(data))
                 return
             checking = checking.get_next()
         else:
-            self.head = Node(data)
+            self.next = Node(data)
 
     # search linked_list for data
     def search(self, data):
-        checking = self.head  # from start point
+        checking = self.next  # from start point
         while checking is not None:
             if checking.get_data() == data:
                 return True
@@ -36,7 +36,7 @@ class LinkedList(object):
 
     # delete node from linked_list
     def remove(self, data):
-        checking = self.head  # search from start
+        checking = self.next  # search from start
         previous = None
 
         while checking is not None:
@@ -46,19 +46,32 @@ class LinkedList(object):
             checking = checking.get_next()
 
         if previous is None:
-            self.head = checking.get_next()
+            self.next = checking.get_next()
         else:
             previous.set_next(checking.get_next())
 
+    def remove_all(self, data):
+        checking = self.next
+        previous = None
+
+        while checking is not None:
+            if checking.get_data() == data:
+                if previous is None:
+                    self.next = checking.get_next()
+                else:
+                    previous.set_next(checking.get_next())
+            previous = checking
+            checking = checking.get_next()
+
 
 def is_empty(self):
-    return self.head is None
+    return self.next is None
 
 
 # the size of linked_list
 def size(self):
     count = 0
-    counting = self.head
+    counting = self.next
     while counting is not None:
         count += 1
         counting = counting.get_next()
@@ -69,7 +82,4 @@ link = LinkedList()
 link.add_head(1)
 link.add_head(2)
 link.add_head(3)
-link.add_tail(4)
-link.add_tail(5)
-link.remove(2)
-
+link.add_head(2)
